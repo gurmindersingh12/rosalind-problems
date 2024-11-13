187467b3f9c5c3de026f5a972aadd881143cb18c
Gurminder Singh <123406976+gurmindersingh12@users.noreply.github.com>
2024-11-13 03:01:31 +0000

    Addede solution for Python-Village

--- Python-Village.py
+++ Python-Village.py
@@ -1,1 +1,19 @@
+## ID: INI1
+
+## Installing Python
+
+
 print ("Hello, World!")
+
+import this
+
+## ID: INI2
+
+## Variables and Some Arithmetic
+
+a = 324
+b = 24
+c = a - b
+print(c)
+
+print ("a - b is", c)
