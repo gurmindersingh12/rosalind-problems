d6c81898295b53cdae2db2d9bb8e94a259fb9588
Gurminder Singh <123406976+gurmindersingh12@users.noreply.github.com>
2024-11-13 03:22:11 +0000

    Added solution for Python-Village

---
+++ Python-Village/Python-Village.py
@@ -0,0 +1,44 @@
+##############################################################################################################
+
+## ID: INI1 (Installing Python)
+
+
+print ("Hello, World!")
+
+import this
+
+#############################################################################################################
+
+## ID: INI2 (Variables and Some Arithmetic)
+
+a = 324
+b = 24
+c = a - b
+print(c)
+
+print ("a - b is", c) 
+
+addition = 2 + 3 == 5
+print('addition is', addition)
+
+subtraction = 5 - 2 == 3
+print('subtraction is', subtraction)
+ 
+
+multiplication = 3 * 4 == 12
+print('multiplication is', multiplication)
+
+
+divison = 15 / 3 == 5
+print('divison is', divison)
+
+
+division_remainder = 18 % 5 == 3
+print('divison_remainder is', division_remainder)
+
+division_remainder1 = float(18) % 5 == 3
+print('divison_remainder1 is', division_remainder1)
+
+exponentiation = 2 ** 3 == 8
+print('exponentiation is', exponentiation)
+
